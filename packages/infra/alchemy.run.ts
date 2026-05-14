6d65c4bce71b32bbcc9525daf55ec1f55a010e0b
sarvesh-astrophile <sarvesh9301@gmail.com>
2026-05-14 22:44:53 +0530

    initial commit

---
+++ packages/infra/alchemy.run.ts
@@ -0,0 +1,19 @@
+import alchemy from "alchemy";
+import { TanStackStart } from "alchemy/cloudflare";
+import { config } from "dotenv";
+
+config({ path: "./.env" });
+config({ path: "../../apps/web/.env" });
+
+const app = await alchemy("reetlab-new");
+
+export const web = await TanStackStart("web", {
+  cwd: "../../apps/web",
+  bindings: {
+    CORS_ORIGIN: alchemy.env.CORS_ORIGIN!,
+  },
+});
+
+console.log(`Web    -> ${web.url}`);
+
+await app.finalize();
